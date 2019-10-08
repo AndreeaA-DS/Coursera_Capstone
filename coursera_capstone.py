038a3db9251e8055718d3b302fef6de8d1fe788d
AndreeaA-DS <56265364+AndreeaA-DS@users.noreply.github.com>
2019-10-08 13:04:38 +0300

    Add files via upload

---
+++ coursera_capstone.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# # These notebook will be mainly used for the capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
